0c1f0e54cf426a02d18f3f8772109e6ba4dc1d42
Solbon Gomboev <solbon.gomboev@qeep.pro>
2023-12-19 14:28:30 +0800

    added swagger

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Swagger\SwaggerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/api/docs', [SwaggerController::class, 'index']);
